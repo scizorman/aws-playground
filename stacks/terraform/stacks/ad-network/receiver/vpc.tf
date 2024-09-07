27c1e5b983a4f33eddfa7beabbd85fa59bcc23c7
scizorman <tueda1207@gmail.com>
2024-09-08 00:23:52 +0900

    Add VPC endpoints for 'ad-network-receiver'

--- stacks/terraform/stacks/ad-network/receiver/vpc.tf
+++ stacks/terraform/stacks/ad-network/receiver/vpc.tf
@@ -23,3 +23,123 @@ module "vpc" {
   flow_log_per_hour_partition         = true
   vpc_flow_log_tags                   = { Name = "${local.name}-vpc-flow-log" }
 }
+
+resource "aws_s3_bucket" "vpc_flow_log" {
+  bucket = local.vpc_flow_log_bucket_name
+
+  tags = {
+    Name = local.vpc_flow_log_bucket_name
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "vpc_flow_log" {
+  bucket = aws_s3_bucket.vpc_flow_log.id
+
+  rule {
+    id     = "30-days-retention"
+    status = "Enabled"
+
+    expiration {
+      days = 30
+    }
+  }
+}
+
+resource "aws_security_group" "vpc_endpoints" {
+  name        = "${local.name}-vpc-endpoints"
+  description = "Security group for VPC endpoints of ${local.name}"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = { Name = "${local.name}-vpc-endpoints" }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "vpc_endpoint_from_vpc_https_ipv4" {
+  security_group_id = aws_security_group.vpc_endpoints.id
+  description       = "Allow inbound HTTPS traffic from all IPv4 addresses within the VPC"
+  cidr_ipv4         = module.vpc.vpc_cidr_block
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "vpc_endpoint_from_vpc_https_ipv6" {
+  security_group_id = aws_security_group.vpc_endpoints.id
+  description       = "Allow inbound HTTPS traffic from all IPv6 addresses within the VPC"
+  cidr_ipv6         = module.vpc.vpc_ipv6_cidr_block
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "vpc_endpoint_to_vpc_all_https_ipv4" {
+  security_group_id = aws_security_group.vpc_endpoints.id
+  description       = "Allow outbound HTTPS traffic to all IPv4 addresses within the VPC"
+  cidr_ipv4         = module.vpc.vpc_cidr_block
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "vpc_endpoint_to_vpc_all_https_ipv6" {
+  security_group_id = aws_security_group.vpc_endpoints.id
+  description       = "Allow outbound HTTPS traffic to all IPv6 addresses within the VPC"
+  cidr_ipv6         = module.vpc.vpc_ipv6_cidr_block
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+}
+
+module "gateway_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "5.13.0"
+
+  vpc_id = module.vpc.vpc_id
+
+  endpoints = {
+    s3 = {
+      service         = "s3"
+      service_type    = "Gateway"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags            = { Name = "${local.name}-s3" }
+    }
+  }
+}
+
+module "interface_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "5.13.0"
+
+  vpc_id             = module.vpc.vpc_id
+  security_group_ids = [aws_security_group.vpc_endpoints.id]
+  subnet_ids         = module.vpc.private_subnets
+
+  endpoints = {
+    ec2 = {
+      service         = "ec2"
+      service_type    = "Interface"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags            = { Name = "${local.name}-ec2" }
+    }
+
+    ec2messages = {
+      service         = "ec2messages"
+      service_type    = "Interface"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags            = { Name = "${local.name}-ec2messages" }
+    }
+
+    ssm = {
+      service         = "ssm"
+      service_type    = "Interface"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags            = { Name = "${local.name}-ssm" }
+    }
+
+    ssmmessages = {
+      service         = "ssmmessages"
+      service_type    = "Interface"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags            = { Name = "${local.name}-ssmmessages" }
+    }
+  }
+}
